2dda7f4b3a287e6161393f911754a4ed84799c42
qisenzhou <qiz080@ucsd.edu>
2017-10-16 12:06:21 -0700

    lates update

--- src/rpc/RpcHelper.java
+++ src/rpc/RpcHelper.java
@@ -53,6 +53,8 @@ public class RpcHelper {
 			out.print(array);
 			out.flush();
 			out.close();
+			// for debugging
+			System.out.println("Response : " + array);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -1,9 +1,9 @@
 package rpc;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,11 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.DBConnectionFactory;
+import db.mysql.MySQLConnection;
 import entity.Item;
-import external.YelpAPI;
+
 
 /**
  * Servlet implementation class SearchItem
@@ -24,12 +26,15 @@ import external.YelpAPI;
 @WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private DBConnection conn = DBConnectionFactory.getDBConnection();
+
+
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public SearchItem() {
-        super();
+        super(); 
         // TODO Auto-generated constructor stub
     }
 
@@ -37,24 +42,32 @@ public class SearchItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		double lat = Double.parseDouble(request.getParameter("lat"));
-		double lon = Double.parseDouble(request.getParameter("lon"));
-		// Term can be empty or null.
-		String term = request.getParameter("term");
-		YelpAPI api = new YelpAPI();
-		List<Item> items = api.search(lat, lon, term);
-		List<JSONObject> list = new ArrayList<>();
-		try {
-			for (Item item : items) {
-				// Add a thin version of item object
-				JSONObject obj = item.toJSONObject();
-				list.add(obj);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		JSONArray array = new JSONArray(list);
-		RpcHelper.writeJsonArray(response, array);
+		 String userId = request.getParameter("user_id");
+		    double lat = Double.parseDouble(request.getParameter("lat"));
+		    double lon = Double.parseDouble(request.getParameter("lon"));
+		    // Term can be empty or null.
+		    String term = request.getParameter("term");
+		    List<Item> items = conn.searchItems(userId, lat, lon, term);
+		    List<JSONObject> list = new ArrayList<>();
+
+		    Set<String> favorite = conn.getFavoriteItemIds(userId);
+		    try {
+		      for (Item item : items) {
+		        // Add a thin version of restaurant object
+		        JSONObject obj = item.toJSONObject();
+		        // Check if this is a favorite one.
+		        // This field is required by frontend to correctly display favorite items.
+		        obj.put("favorite", favorite.contains(item.getItemId()));
+
+		        list.add(obj);
+		      }
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    }
+		    JSONArray array = new JSONArray(list);
+		    RpcHelper.writeJsonArray(response, array);
+
+
 
 	}
 
